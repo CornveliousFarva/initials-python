7fa2e9d7f3820f40b916125470cb2cfd5a63186d
Greg Schuman <gcschuman@hotmail.com>
2022-09-23 12:07:45 -0700

    Added initial letters.

---
+++ initials.py
@@ -0,0 +1,32 @@
+# First initial, made by storing the letter in a variable, using triple quotation marks 
+name = """
+  GGG
+G     G
+G
+GGGGGGG
+G     G
+G     G
+  GGG
+"""
+print(name)
+
+# Last initial, made by printing each initial line
+print("  SSS  ")
+print(" S   S ")
+print(" S     ")
+print("  SSS  ")
+print("     S ")
+print(" S   S ")
+print("  SSS  ")
+
+# A couple of extra letters
+h = """
+H       H
+H       H
+H       H
+HHHHHHHHH
+H       H
+H       H
+H       H
+"""
+print(h)
